4fbe8de090979be4267166de2072cbb849e161e0
Dmitry Batsenko <batsenkod@gmail.com>
2019-06-07 11:50:09 +0300

    Updates

---
+++ precache-manifest.035e49263e38edfb62df4e207d4ca893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4f106b47d68dbb69fa9cbe91cf80be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bac8aa3dcebb41aafc34",
+    "url": "./static/css/main.c244cbac.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "./static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bac8aa3dcebb41aafc34",
+    "url": "./static/js/main.dc05ec47.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
